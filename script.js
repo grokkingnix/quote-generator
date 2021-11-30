51e4119b4e1ceeadcf0f8970b130a5ed0212f661
grokkingnix <nix@nixing.mx>
2021-11-30 00:07:34 -0600

    Add showing of quote on the DOM

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
+// Get DOM elements
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
 let apiQuotes = [];
 
 // Show new Quote
 const newQuote = () => {
   // Pick a random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  console.log(quote);
+
+  // Check if quote has author, if not pass in "Unknown"
+  const defineAuthorName = (authorValue) => {
+    const authorName = authorValue ? authorValue : "Unkown";
+    return authorName;
+  };
+
+  // Send data to DOM
+  authorText.textContent = defineAuthorName(quote.author);
+  quoteText.textContent = quote.text;
 };
 
 // Get quotes from API
